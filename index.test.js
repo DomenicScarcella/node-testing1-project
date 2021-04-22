a5de5f88a39b6ebaea8f2f848ba8c95000b39978
Domenic Scarcella <ls41513@yahoo.com>
2021-04-21 22:05:16 -0400

    Slowly getting some tests to pass

--- index.test.js
+++ index.test.js
@@ -1,6 +1,7 @@
 const utils = require('./index');
 
 describe('[Exercise 1] trimProperties', () => {
+
   test('[1] returns an object with the properties trimmed', () => {
     // EXAMPLE
     const input = { foo: '  foo ', bar: 'bar ', baz: ' baz' };
@@ -8,16 +9,37 @@ describe('[Exercise 1] trimProperties', () => {
     const actual = utils.trimProperties(input);
     expect(actual).toEqual(expected);
   });
-  test.todo('[2] returns a copy, leaving the original object intact');
+
+  test('[2] returns a copy, leaving the original object intact', () => {
+    const input = { foo: '  foo ', bar: 'bar ', baz: ' baz' };
+    utils.trimProperties(input);
+    expect(input).toEqual({ foo: '  foo ', bar: 'bar ', baz: ' baz' });
+  });
 });
 
 describe('[Exercise 2] trimPropertiesMutation', () => {
-  test.todo('[3] returns an object with the properties trimmed');
-  test.todo('[4] the object returned is the exact same one we passed in');
+
+  test('[3] returns an object with the properties trimmed', () => {
+    const input = { foo: '  foo ', bar: 'bar ', baz: ' baz' };
+    const expected = { foo: 'foo', bar: 'bar', baz: 'baz' };
+    const actual = utils.trimProperties(input);
+    expect(actual).toEqual(expected);
+  });
+
+  test('[4] the object returned is the exact same one we passed in', () => {
+    const input = { foo: '  foo ', bar: 'bar ', baz: ' baz' };
+    const actual = utils.trimPropertiesMutation(input);
+    expect(actual).toBe(input);
+  });
 });
 
 describe('[Exercise 3] findLargestInteger', () => {
-  test.todo('[5] returns the largest number in an array of objects { integer: 2 }');
+
+  test('[5] returns the largest number in an array of objects { integer: 2 }', () => {
+    const input = [{ integer: 1 }, { integer: 3 }, { integer: 2 }];
+    const actual = utils.findLargestInteger(input);
+    expect(actual).toBe(3);
+  });
 });
 
 describe('[Exercise 4] Counter', () => {
@@ -25,9 +47,23 @@ describe('[Exercise 4] Counter', () => {
   beforeEach(() => {
     counter = new utils.Counter(3); // each test must start with a fresh couter
   });
-  test.todo('[6] the FIRST CALL of counter.countDown returns the initial count');
-  test.todo('[7] the SECOND CALL of counter.countDown returns the initial count minus one');
-  test.todo('[8] the count eventually reaches zero but does not go below zero');
+
+  test('[6] the FIRST CALL of counter.countDown returns the initial count', () => {
+    expect(counter.countDown()).toBe(3);
+  });
+
+  test('[7] the SECOND CALL of counter.countDown returns the initial count minus one', () => {
+    counter.countDown();
+    expect(counter.countDown()).toBe(2);
+  });
+
+  test('[8] the count eventually reaches zero but does not go below zero', () => {
+    counter.countDown();
+    counter.countDown();
+    counter.countDown();
+    counter.countDown();
+    expect(counter.countDown()).toBe(0);
+  });
 });
 
 describe('[Exercise 5] Seasons', () => {
